b9f74f2a0d594739e70ac9e28a35e7969cc3ab20
Japhet Cordova <vraxx07@gmail.com>
2025-07-21 11:38:14 +0800

    02: themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,27 @@
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 
 export default function Home() {
-const { setTheme } = useTheme()
+const { setTheme, theme } = useTheme();
   return (
-    <Card className="items-center-safe content-center ">
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+    <div className="flex items-center justify-center h-screen">
+      <Card className="mx-8 md:mx-80 p-4">
+
+        <Button 
+          variant="outline"
+          size="icon"
+          onClick={() => {
+            setTheme(theme === "light" ? "dark" : "light");
+          }}
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-    </Card>
+
+      </Card>
+    </div>
   );
 }
