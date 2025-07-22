89c37c4a4040a58f9cef1a4cfabdb09903f87840
Japhet Cordova <vraxx07@gmail.com>
2025-07-22 20:26:07 +0800

    resolved

--- src/components/profileBody.tsx
+++ src/components/profileBody.tsx
@@ -1,9 +1,9 @@
 import JaphCard from "./japhcard"
 export const ProfileBody = () =>{
     return(
-        <div className="flex flex-col gap-y-2 w-full flex-w mt-4">
+        <div className="flex flex-col gap-y-2 w-full mt-4">
             <div className="flex flex-col md:flex-row gap-x-2 w-full">
-                <div className="flex flex-col flex-1 gap-y-2 basis-100">
+                <div className="flex flex-col flex-1 gap-y-2 basis-1">
                     <div className="basis-1/2">
                         <JaphCard/>
                     </div>
@@ -30,7 +30,7 @@ export const ProfileBody = () =>{
                     <JaphCard/>
                 </div>  
                 
-                <div className="flex-1">
+                <div className="flex-3">
                     <JaphCard/>
                 </div>                 
             </div>
