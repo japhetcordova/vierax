a34ad3bd5daa8f8a4b36b6333f41106f1114ab2f
Japhet Cordova <vraxx07@gmail.com>
2025-07-28 21:50:43 +0800

    Fix invalid Tailwind border class.

--- src/app/profile/profileFooter.tsx
+++ src/app/profile/profileFooter.tsx
@@ -2,7 +2,7 @@
 
 export const ProfileFooter = () =>{
     return(
-        <div className="border-t-1 mt-8 mb-4 border-accent flex justify-between">
+        <div className="border-t mt-8 mb-4 border-accent flex justify-between">
             <p className="text-xs p-2">© 2025 Japhet Cordova. All rights reserved.</p>
             <p className="text-xs p-2">Feedback</p>
         </div>
